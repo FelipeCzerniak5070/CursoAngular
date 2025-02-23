fce95e35b3a6651a87ef5f8090ef73b6e872a6f7
Felipe Czerniak <felipeczerniak@gmail.com>
2025-02-23 19:58:26 -0300

    FormsModule 1

--- src/app/components/1-testes/c1/c1.component.ts
+++ src/app/components/1-testes/c1/c1.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, NgModule } from '@angular/core';
+import { FormsModule, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-c1',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './c1.component.html',
   styleUrl: './c1.component.css'
 })
 export class C1Component {
   nomes:string[] = ["Felipe", "Mary", "Chimia", "Pelica"];
   linguagem:string = "HTMl";
+  nome:string = "";
 }
