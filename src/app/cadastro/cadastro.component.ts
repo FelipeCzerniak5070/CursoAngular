ae9525d5773d2306fd76300dd80a5cfcaada0221
Felipe Czerniak <felipeczerniak@gmail.com>
2025-02-26 22:10:18 -0300

    Função de Cadastro

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Pessoa } from "../Model/Pessoa";
 
 @Component({
   selector: 'app-cadastro',
@@ -12,11 +13,23 @@ export class CadastroComponent {
   form = new FormGroup({
    nome: new FormControl('',[Validators.required,Validators.minLength(10),Validators.maxLength(255)]),
    idade: new FormControl(null,[Validators.required,Validators.min(12),Validators.max(120)]),
-   cidade: new FormControl('',[Validators.required,Validators.minLength(10),Validators.maxLength(255)])
+   cidade: new FormControl('',[Validators.required,Validators.minLength(3),Validators.maxLength(255)])
   });
 
   btnCadastrar:boolean = true;
-  
+
+  pessoas:Pessoa[] = []; 
+
+  // Função de Cadastro
+
+  cadastrarCliente(){
+    this.pessoas.push(this.form.value as Pessoa);
+
+    this.form.reset();
+
+    console.table(this.pessoas);
+
+  }
 
 }
 
