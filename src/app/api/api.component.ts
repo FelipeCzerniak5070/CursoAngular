c76f729a4ace34c97a3a51e2a97b664298afe26e
Felipe Czerniak <felipeczerniak@gmail.com>
2025-02-27 22:42:07 -0300

    Listar Produtos

--- src/app/api/api.component.ts
+++ src/app/api/api.component.ts
@@ -1,11 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Produto } from '../Model/Produto';
+import { ProdutoService } from '../service/produto.service';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-api',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './api.component.html',
   styleUrl: './api.component.css'
 })
 export class ApiComponent {
+
+  constructor(private service:ProdutoService){}
+
+  //Vetor de Produtos da API
+  produtos:Produto[]=[];
+
+  // Inicialização do Component
+  ngOnInit(){
+    this.selecionarProduto()
+  }
+
+  // Método de Seleção de Produtos
+
+  selecionarProduto(){
+    this.service.selecionar().subscribe(retorno => this.produtos = retorno);
+
+  }
 
 }
