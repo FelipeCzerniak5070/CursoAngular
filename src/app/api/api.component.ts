63431e25bc3c458da1d410a9ecacf4344b30a261
Felipe Czerniak <felipeczerniak@gmail.com>
2025-02-27 22:52:31 -0300

    Formulário Reativo

--- src/app/api/api.component.ts
+++ src/app/api/api.component.ts
@@ -2,17 +2,28 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Produto } from '../Model/Produto';
 import { ProdutoService } from '../service/produto.service';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-api',
-  imports: [CommonModule],
+  imports: [CommonModule,ReactiveFormsModule],
   templateUrl: './api.component.html',
   styleUrl: './api.component.css'
 })
 export class ApiComponent {
 
   constructor(private service:ProdutoService){}
+
+  // Objeto de Formulário
+
+  form= new FormGroup({
+    id: new FormControl(null),
+    nome: new FormControl(""),
+    valor: new FormControl(null)
+  });
+
+  //Botões Visíveis
+  btnCadastrarVisivel:boolean=true;
 
   //Vetor de Produtos da API
   produtos:Produto[]=[];
